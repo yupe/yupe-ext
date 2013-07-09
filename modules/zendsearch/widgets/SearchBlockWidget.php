d57d5a53f02f3f186f100406600ca0964e3064fe
yupe <aopeykin@yandex.ru>
2013-07-09 22:25:58 +0400

    zendsearch moved to main repo

--- modules/zendsearch/widgets/SearchBlockWidget.php
+++
@@ -1,9 +0,0 @@
-<?php
-class SearchBlockWidget extends YWidget
-{
-
-    public function run()
-    {
-        $this->render('searchblockwidget', array());
-    }
-}
--- modules/zendsearch/widgets/views/searchblockwidget.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo CHtml::beginForm(array('/zendsearch/search/search'), 'post', array('style' => 'inline')) .
-        CHtml::textField('q', '', array('placeholder' => 'Поиск...', 'class' => '')) .
-        CHtml::submitButton('', array('class'=>'btn')) .
-        CHtml::endForm('');
-?>
