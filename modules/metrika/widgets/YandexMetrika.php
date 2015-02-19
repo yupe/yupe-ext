4d16ceeb3d37e245cfb33746d0573eb82d0d39d3
apexwire <apexwire@gmail.com>
2015-02-19 12:30:57 +0400

    виджет YandexMetrika 1) перенесен в модуль Metrika 2) добавлен комментарий модуля 3) изменена ссылка на "родителя"

--- modules/metrika/widgets/YandexMetrika.php
+++ modules/metrika/widgets/YandexMetrika.php
@@ -1,6 +1,17 @@
 <?php
 
-class YandexMetrika extends YWidget
+/**
+ * Виджет для вывода Yandex Metrika
+ *
+ * @category YupeWidget
+ * @package  yupe.modules.metrika.widgets
+ * @author   Opeykin A. <aopeykin@yandex.ru>
+ * @license  BSD http://ru.wikipedia.org/wiki/%D0%9B%D0%B8%D1%86%D0%B5%D0%BD%D0%B7%D0%B8%D1%8F_BSD
+ * @link     http://yupe.ru
+ *
+ **/
+
+class YandexMetrika extends yupe\widgets\YWidget
 {
     public $counter;
     public $informer = true;
